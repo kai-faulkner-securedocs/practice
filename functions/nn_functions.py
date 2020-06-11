e2c2ffc4772fe1000acb08260a1924278e82a8fe
kai-faulkner-securedocs <kai.faulkner@securedocs.com>
2020-06-11 15:59:56 -0700

    functions added

--- functions/nn_functions.py
+++ functions/nn_functions.py
@@ -36,23 +36,35 @@ def build_model(dim):
 	model = Sequential()
 	model.add(Embedding(5000, 256, input_length=dim))
 	model.add(Flatten())
-	model.add(Dense(80, activation='relu'))
-	model.add(Dense(70, activation='relu'))
+	model.add(Dense(50, activation='relu'))
+	model.add(Dense(50, activation='relu'))
 	model.add(Dropout(.4))
 	model.add(Dense(20, activation='relu'))
 	model.add(Dense(10, activation='relu'))
-	model.add(Dense(30, activation='relu'))
+	model.add(Dense(10, activation='relu'))
+	model.add(Dense(10, activation='relu'))
 	model.add(Dense(2, activation='softmax'))
 	model.compile(optimizer='adam',
 		loss='categorical_crossentropy',
 		metrics=['accuracy'])
 	return model
 
-def train_model(model, x, y, epoch=5, batch=32):
+def train_model(model, x, y, epoch=3, batch=64):
 	from tensorflow import keras
 	model.fit(x, y, epochs=epoch, batch_size=batch)
 	return model
+	#save the weights of the model as a json file and load it
 
 def evaluate_model(model, x, y):
 	from tensorflow import keras
 	return model.evaluate(x, y)
+
+def save_model(model, loc): 
+	model.save(loc)
+	print(f'saved model to {loc}')
+
+def import_model(loc):
+	from keras.models import load_model
+	model = load_model(loc)
+	print(f'loaded model from {loc}')
+	return model
